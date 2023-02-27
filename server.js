1d70ea0e106e36b595824515c38384af8d7c465b
joshyahweh <joshojoawo@gmail.com>
2023-02-27 14:06:00 +0100

    ForgotPassword and SignOut Auth

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import session from "express-session";
-import signupRouter from "./src/routes/auth/signup.js";
-import signedOutRouter from "./src/routes/auth/signout.js";
-import signedInRouter from "./src/routes/auth/signIn.js";
 import { protect } from "./src/modules/auth.js";
-import { createNewUser, signin } from "./src/handlers/users.js";
+import {
+  createNewUser,
+  forgotPassword,
+  signin,
+  signout,
+} from "./src/handlers/users.js";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +21,8 @@ app.use(
 const PORT = 3002;
 app.post("/api/v1/signup", createNewUser);
 app.post("/api/v1/signin", signin);
+app.post("/api/v1/forgotPassword", forgotPassword);
+app.post("/api/v1/signout", signout);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING AT PORT ${PORT}`);
--- src/handlers/users.js
+++ src/handlers/users.js
@@ -2,6 +2,7 @@ import { json } from "express";
 import prisma from "../db.js";
 import { comparePassword, createJWT, hashedPassword } from "../modules/auth.js";
 
+// New User Authentication
 export const createNewUser = async (req, res) => {
   const user = await prisma.user.create({
     data: {
@@ -16,6 +17,7 @@ export const createNewUser = async (req, res) => {
   res.json({ token });
 };
 
+// Sign in
 export const signin = async (req, res) => {
   const user = await prisma.user.findUnique({
     where: {
@@ -30,4 +32,53 @@ export const signin = async (req, res) => {
   }
   const token = createJWT(user);
   res.json({ token });
+};
+
+// Forgot Password
+export const forgotPassword = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid username" });
+  }
+
+  // Check if new password matches confirm password
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+  const updatedUser = await prisma.user.update({
+    where: { username: req.body.username },
+    data: { password: await hashedPassword(req.body.newPassword) },
+  });
+  // Update user's password
+
+  const token = createJWT(updatedUser);
+  res.json({ token });
+};
+
+// Sign out
+export const signout = async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  // Verify the token
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+
+  // Get the user ID from the token payload
+  const payload = jwt.decode(token);
+  const userId = payload.sub;
+
+  // Update the user's token version to invalidate the current token
+  const user = await prisma.user.update({
+    where: { id: userId },
+    data: { tokenVersion: { increment: 1 } },
+  });
+
+  return res.status(200).json({ message: "Sign out successful" });
 };
